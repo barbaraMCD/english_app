32ce3ff721eb7a5fcc7f9b90f0a13a89d2a01950
barbara rtweb <102307204+barbaraMCD@users.noreply.github.com>
2023-02-17 12:25:09 +0100

    install redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
